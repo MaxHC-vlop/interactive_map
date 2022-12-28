e3072f4be0b523092d2c61086ea0355c8bde020f
Vladuslav Dragushin <maxhc@inbox.ru>
2022-12-28 21:08:53 +0300

    Fix Place model .

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -5,11 +5,11 @@ class Place(models.Model):
     title = models.CharField(
         max_length=100, verbose_name='Название'
     )
-    description_short = models.CharField(
+    description_short = models.TextField(
         max_length=255, verbose_name='Краткое описание', blank=True
     )
-    description_long = models.CharField(
-        max_length=255, verbose_name='Полное описание', blank=True
+    description_long = models.TextField(
+        verbose_name='Полное описание', blank=True
     )
     lng = models.FloatField(verbose_name='Широта')
     lat = models.FloatField(verbose_name='Долгота')
