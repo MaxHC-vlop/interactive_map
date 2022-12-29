55e990c8f8b688a75a011ed6b32301fd26dfe518
Vladuslav Dragushin <maxhc@inbox.ru>
2022-12-29 12:02:29 +0300

    Took locations from the database .

--- where_to_go/where_to_go/views.py
+++ where_to_go/where_to_go/views.py
@@ -1,36 +1,32 @@
+from places.models import Place
 from django.shortcuts import render
 
 
 def show_index(request):
-    places = {
-        "type": "FeatureCollection",
-        "features": [
+    places = Place.objects.all()
+    features = []
+    for place in places:
+        features.append(
             {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [
+                        place.lng, place.lat
+                    ]
                 },
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "static/places/moscow_legends.json"
-                }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "static/places/roofs24.json"
+                'properties': {
+                    'title': place.title,
+                    'placeId': place.id,
+                    'detailsUrl': 'static/places/moscow_legends.json'
                 }
             }
-        ]
+        )
+    feature_collection = {
+        'type': 'FeatureCollection',
+        'features': features
     }
 
-    context = {'places': places}
+    context = {'feature_collection': feature_collection}
+
     return render(request, 'index.html', context)
