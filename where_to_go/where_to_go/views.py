fa0e06e31b3fc36d9a5ed4292e51ad0b46fefb17
Vladuslav Dragushin <maxhc@inbox.ru>
2022-12-29 12:41:27 +0300

    Add JSON upload

--- where_to_go/where_to_go/views.py
+++ where_to_go/where_to_go/views.py
@@ -1,6 +1,6 @@
 from places.models import Place
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 def show_index(request):
     places = Place.objects.all()
@@ -36,5 +36,13 @@ def places(request, place_id=1):
     place = get_object_or_404(Place, pk=place_id)
     place_context = {
             'title': place.title,
+            'imgs': [img.photo.url for img in place.images.all().order_by('sort_index')],
+            'description_short': place.description_short,
+            'description_long': place.description_long,
+            'coordinates': {
+                'lat': place.lat,
+                'lng': place.lng
             }
-    return HttpResponse(place_context['title'])
+        }
+
+    return JsonResponse(place_context, json_dumps_params={'ensure_ascii': False})
