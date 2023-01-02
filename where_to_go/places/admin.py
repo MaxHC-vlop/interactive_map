e5c9cadedda2ac00e0b701c05716759937d5da01
Vladuslav Dragushin <maxhc@inbox.ru>
2023-01-02 14:41:52 +0300

    Added inline Image to Admin

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from places.models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
 @admin.register(Place)
 class PlaceAdminModel(admin.ModelAdmin):
-    pass
+    fieldsets = [
+        (None,         {'fields': ['title']}),
+        ('Описание',   {'fields': ['description_short', 'description_long'], 'classes': ['collapse']}),
+        ('Координаты', {'fields': ['lat', 'lng'], 'classes': ['collapse']}),
+    ]
+    inlines = [ImageInline]
 
 
 @admin.register(Image)
