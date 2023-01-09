a94bb1497144d227be00e5de4f134117e7520839
Vladuslav Dragushin <maxhc@inbox.ru>
2023-01-09 15:37:49 +0300

    Fix upload_image

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -1,24 +1,24 @@
-import requests
-
-from django.core.management.base import BaseCommand
 from urllib.parse import urlparse
 from io import BytesIO
 
+import requests
+
+from django.core.files.base import ContentFile
+from django.core.management.base import BaseCommand
 from places.models import Place, Image
 
 
 def upload_image(image, number, place):
-    image_name = urlparse(image).path.split('/')[-1]
+    filename = urlparse(image).path.split('/')[-1]
     response = requests.get(image)
     response.raise_for_status()
-    image, created = Image.objects.get_or_create(
+
+    image = BytesIO(response.content)
+    photo = ContentFile(image.read(), name=filename)
+    Image.objects.create(
         sort_index=number,
         place=place,
-    )
-    image.photo.save(
-        f'{place.id}_{image_name}',
-        BytesIO(response.content),
-        save=True
+        photo=photo
     )
 
 
@@ -62,5 +62,13 @@ class Command(BaseCommand):
                     self.stdout.write(f'Load {image}')
 
             except KeyError as error:
+                self.stdout.write(f'Error load: {error}')
+                continue
+
+            except requests.exceptions.HTTPError as error:
+                self.stdout.write(f'Error load: {error}')
+                continue
+
+            except requests.exceptions.ConnectionError as error:
                 self.stdout.write(f'Error load: {error}')
                 continue
