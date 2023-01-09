704a86c0f51f12d788e91a62e5461fab51a1d790
Vladuslav Dragushin <maxhc@inbox.ru>
2023-01-09 15:28:40 +0300

    Highlighted a separate function in commands

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -7,6 +7,21 @@ from io import BytesIO
 from places.models import Place, Image
 
 
+def upload_image(image, number, place):
+    image_name = urlparse(image).path.split('/')[-1]
+    response = requests.get(image)
+    response.raise_for_status()
+    image, created = Image.objects.get_or_create(
+        sort_index=number,
+        place=place,
+    )
+    image.photo.save(
+        f'{place.id}_{image_name}',
+        BytesIO(response.content),
+        save=True
+    )
+
+
 class Command(BaseCommand):
     help = 'Getting data from json.'
 
@@ -24,6 +39,7 @@ class Command(BaseCommand):
             response = requests.get(url)
             response.raise_for_status()
             place_content = response.json()
+
             try:
                 place, created = Place.objects.update_or_create(
                     title=place_content['title'],
@@ -39,23 +55,12 @@ class Command(BaseCommand):
                     }
                 )
                 self.stdout.write(f'Load {place.title} {created}')
-                for num, img in enumerate(place_content.get('imgs'), start=1):
-                    img_name = urlparse(img).path.split('/')[-1]
-                    response = requests.get(img)
-                    response.raise_for_status()
-                    image, _created = Image.objects.get_or_create(
-                        sort_index=num,
-                        place=place,
-                    )
-                    image.photo.save(
-                        f'{place.id}_{img_name}',
-                        BytesIO(response.content),
-                        save=True
-                    )
-                    self.stdout.write(f'Load {image.photo.url}')
+
+                images = place_content.get('imgs')
+                for number, image in enumerate(images, start=1):
+                    upload_image(image, number, place)
+                    self.stdout.write(f'Load {image}')
+
             except KeyError as error:
                 self.stdout.write(f'Error load: {error}')
                 continue
-
-            except Exception as error:
-                self.stdout.write(f'Error load: {error}')
